3d98c5453206f8cc2d4ae5e3d9495a1d31db47e1
Alex Matheus de Paula <76526930+AlexMathss@users.noreply.github.com>
2024-08-06 21:49:55 -0300

    Página de serviços

--- src/Pages/Product/index.js
+++ src/Pages/Product/index.js
@@ -9,6 +9,13 @@ import logo_2 from '../../imagens/logo_2.png';
 import logo_3 from '../../imagens/logo_3.png';
 import logo_4 from '../../imagens/logo_4.png';
 import logo_5 from '../../imagens/logo_5.png';
+// ------------------
+import security from '../../imagens/1.svg';
+import features from '../../imagens/2.svg';
+import collaborate from '../../imagens/3.svg';
+import sync from '../../imagens/4.svg';
+import ia from '../../imagens/5.svg';
+import notes from '../../imagens/6.svg';
 
 function Product(){
     return(
@@ -44,6 +51,65 @@ function Product(){
                     <Image src={logo_5}/>
                 </Box>
                 
+            </Container>
+
+            <Container className="box-2">
+                <Container className="service-container">
+                    <Box className="box-title">
+                        <Text style={{fontSize: 48, fontWeight: "bold"}}>
+                            Tire o melhor proveito da sua empresa<br/> com o nosso serviço
+                        </Text>
+                    </Box>
+
+                    <Box className="service-box">
+                        <Container className="grid-container">
+                            <Box className="service-description">
+                                <Image src={security} className="img-description"/>
+                                <Text className="service-title">Segurança e Privacidade</Text>
+                                <Text className="service-span">
+                                    Garantindo a máxima segurança contra ameaças cibernéticas.
+                                </Text>
+                            </Box>
+                            <Box className="service-description">
+                                <Image src={features} className="img-description"/>
+                                <Text className="service-title">Recursos IA Integrados</Text>
+                                <Text className="service-span">
+                                    Oferece personalização avançada e automação inteligente para processos empresariais.
+                                </Text>
+                            </Box>
+                            <Box className="service-description">
+                                <Image src={collaborate} className="img-description"/>
+                                <Text className="service-title">Colaborar com outros</Text>
+                                <Text className="service-span">
+                                    Ferramentas intuitivas para facilitar o trabalho em equipe de maneira eficiente
+                                </Text>
+                            </Box>
+                        </Container>
+                        <Container className="grid-container">
+                            <Box className="service-description">
+                                <Image src={sync} className="img-description"/>
+                                <Text className="service-title">Sincronização em tempo real</Text>
+                                <Text className="service-span">
+                                    Assegura que todos os dados e arquivos estejam atualizados em todos os dispositivos.
+                                </Text>
+                            </Box>
+                            <Box className="service-description">
+                                <Image src={ia} className="img-description"/>
+                                <Text className="service-title">NovaLogic IA</Text>
+                                <Text className="service-span">
+                                    É uma avançado produto de inteligência artificial projetado para transformar e otimizar processos.
+                                </Text>
+                            </Box>
+                            <Box className="service-description">
+                                <Image src={notes} className="img-description"/>
+                                <Text className="service-title">Fácil organização de notas</Text>
+                                <Text className="service-span">
+                                    Permite capturar, classificar e acessar informações rapidamente, garantindo que nada seja perdido.
+                                </Text>
+                            </Box>
+                        </Container>
+                    </Box>
+                </Container>
             </Container>
 
         </div>
